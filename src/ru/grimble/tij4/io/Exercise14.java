d162c0a3768a4d9e1f4bc63ea485458aec80af0a
grimble <grimble@gmx.com>
2019-05-29 22:57:10 +0700

    Buffered and plain file writing compare OS buffering matters. Second call is always faster. When called separately buffered is slightly faster.

---
+++ src/ru/grimble/tij4/io/Exercise14.java
@@ -0,0 +1,51 @@
+package ru.grimble.tij4.io;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.Random;
+
+public class Exercise14 {
+
+    static long measureWriter(PrintWriter pw) {
+
+        Random r= new Random(47);
+
+        Date start= new Date();
+
+        for (int i=0; i < 10000 ; i++) {
+            StringBuffer sb= new StringBuffer();
+            for (int j=0; j < 1000; j++) {
+                sb.append((char)(r.nextInt(122-32) + 32));
+            }
+            pw.println(sb.toString());
+        }
+
+        pw.close();
+
+        Date end= new Date();
+
+        return end.getTime() - start.getTime();
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        long buffered= measureWriter(
+                new PrintWriter(
+                        new BufferedWriter(
+                                new FileWriter("buffered.txt"))));
+
+        long plain= measureWriter(
+                new PrintWriter(
+                        new FileWriter("plain.txt")));
+
+        System.out.format("Buffered   %d\n", buffered);
+        System.out.format("Plain      %d\n", plain);
+        System.out.format("Difference %d\n", plain - buffered);
+
+    }
+
+}
