6915452c786495f6076bd59b27fd4b3640ce2118
grimble <grimble@gmx.com>
2019-06-05 00:11:57 +0700

    IO methods root names with data types processed in a class

--- src/ru/grimble/tij4/io/Exercise15.java
+++ src/ru/grimble/tij4/io/Exercise15.java
@@ -1,16 +1,29 @@
 package ru.grimble.tij4.io;
 
+import sun.awt.image.ImageWatched;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.lang.reflect.Method;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Exercise15 {
 
+    static class DataIOMethod {
+
+        String name;
+        Class dataType;
+
+        public DataIOMethod(String name, Class dataType) {
+            this.name=name;
+            this.dataType=dataType;
+        }
+
+    }
+
     public static void main(String[] args) {
 
-        Set<Class> dataTypes= new HashSet<Class>();
+        List<DataIOMethod> dataIOMethods= new LinkedList<DataIOMethod>();
 
         for (Method method : DataOutputStream.class.getMethods()) {
 
@@ -31,10 +44,16 @@ public class Exercise15 {
             if (!methodName.startsWith("write"))
                 continue;
 
-            dataTypes.add(dataType);
+            dataIOMethods.add(new DataIOMethod(methodName.replaceFirst("write", ""), dataType));
 
-            System.out.format("%s(%s)\n", methodName, dataType.getSimpleName());
+        }
 
+        Iterator<DataIOMethod> dataIOMethodIterator= dataIOMethods.iterator();
+        System.out.format("%s %s\n", "Name", "Data type");
+        System.out.format("--------------------------------\n");
+        while (dataIOMethodIterator.hasNext()) {
+            DataIOMethod dataIOMethod= dataIOMethodIterator.next();
+            System.out.format("%s %s\n", dataIOMethod.name, dataIOMethod.dataType.getSimpleName());
         }
 
     }
