0e0f78a8ff419ba63a50fcd716d716cc076ea4a5
grimble <grimble@gmx.com>
2019-06-05 08:04:19 +0700

    Data input stream corresponding methods, all packed in a class

--- src/ru/grimble/tij4/io/Exercise15.java
+++ src/ru/grimble/tij4/io/Exercise15.java
@@ -1,7 +1,5 @@
 package ru.grimble.tij4.io;
 
-import sun.awt.image.ImageWatched;
-
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.lang.reflect.Method;
@@ -9,10 +7,22 @@ import java.util.*;
 
 public class Exercise15 {
 
+    /**
+     * Data IO streams read and write companion methods
+     */
     static class DataIOMethod {
 
         String name;
         Class dataType;
+        Method readMethod;
+        Method writeMethod;
+
+        public DataIOMethod(String name, Class dataType, Method readMethod, Method writeMethod) {
+            this.name=name;
+            this.dataType=dataType;
+            this.readMethod=readMethod;
+            this.writeMethod=writeMethod;
+        }
 
         public DataIOMethod(String name, Class dataType) {
             this.name=name;
@@ -21,41 +31,124 @@ public class Exercise15 {
 
     }
 
-    public static void main(String[] args) {
+    /**
+     * Finds data io streams r/w companion methods
+     * @return  a list of methods that read and write the same data type
+     */
+    static List<DataIOMethod> getDataIOMethods() {
 
         List<DataIOMethod> dataIOMethods= new LinkedList<DataIOMethod>();
 
-        for (Method method : DataOutputStream.class.getMethods()) {
+        for (Method writeMethod : DataOutputStream.class.getMethods()) {
 
             // skip inhereted methods
-            if (method.getDeclaringClass() != DataOutputStream.class)
+            if (writeMethod.getDeclaringClass() != DataOutputStream.class)
                 continue;
 
-            Class[] parameterTypes= method.getParameterTypes();
+            Class[] parameterTypes= writeMethod.getParameterTypes();
 
             // skip methods with no parameters
             if (parameterTypes.length == 0)
                 continue;
 
             Class dataType= parameterTypes[0];
-            String methodName= method.getName();
+            String methodName= writeMethod.getName();
 
             // process only methods that write smth
             if (!methodName.startsWith("write"))
                 continue;
 
-            dataIOMethods.add(new DataIOMethod(methodName.replaceFirst("write", ""), dataType));
+            String name= methodName.replaceFirst("write", "");
+
+            Method readMethod;
+            try {
+                readMethod=DataInputStream.class.getDeclaredMethod("read" + name);
+            } catch (NoSuchMethodException e) {
+                continue;
+            }
+
+            dataIOMethods.add(new DataIOMethod(
+                    name,
+                    dataType,
+                    readMethod,
+                    writeMethod
+            ));
 
         }
 
+        return dataIOMethods;
+    }
+
+    public static void main(String[] args) {
+
+        List<DataIOMethod> dataIOMethods= getDataIOMethods();
+
+        // todo: write data
+        // todo: read data
+        // todo: verify read data
+
         Iterator<DataIOMethod> dataIOMethodIterator= dataIOMethods.iterator();
-        System.out.format("%s %s\n", "Name", "Data type");
-        System.out.format("--------------------------------\n");
+
         while (dataIOMethodIterator.hasNext()) {
+
             DataIOMethod dataIOMethod= dataIOMethodIterator.next();
-            System.out.format("%s %s\n", dataIOMethod.name, dataIOMethod.dataType.getSimpleName());
+
+            System.out.format("%s: input %s, output %s\n",
+                    dataIOMethod.dataType.getSimpleName(),
+                    dataIOMethod.readMethod.getName(),
+                    dataIOMethod.writeMethod.getName()
+            );
+
         }
 
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
